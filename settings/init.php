444fa1f455695b5d8462daad13d6ccb5752ee3ed
boertel <ben@punchtab.com>
2012-06-02 16:17:22 -0700

    Revert "change the way we detect if it's prod or dev:"
    This reverts commit 7c9887d605a1e49d128e6d5876b3c58876b822ee.

--- settings/init.php
+++ settings/init.php
@@ -2,12 +2,12 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT']);
 
-if ($_SERVER['HTTP_HOST'] == "eurofoot-boertel.dotcloud.com") {
-    require 'settings/prod.php';
-    require 'settings/prod.private.php';
-} else {
+if ($_SERVER['REMOTE_ADDR'] == "127.0.0.1") {
     require 'settings/dev.php';
     require 'settings/dev.private.php';
+} else {
+    require 'settings/prod.php';
+    require 'settings/prod.private.php';
 }
 
 require 'facebook-sdk/facebook.php';
